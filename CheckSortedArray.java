523b04677e11dfdae9714d83a006d06cb045ee94
SSonikaa <sainusonika@gmail.com>
2025-01-12 11:55:56 +0530

    4th problem

---
+++ CheckSortedArray.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class CheckSortedArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your array size");
+        int n = sc.nextInt();
+        int [] arr = new int [n];
+        int [] copyarr = new int [n];
+        System.out.println("Enter your array elements");
+        for(int i = 0 ; i< arr.length ; i++){
+            arr[i] = sc.nextInt();
+            copyarr[i] = arr[i];
+        }
+        Arrays.sort(arr);
+        int count =0;
+        for(int j = 0 ; j< n ;j++){
+            if(copyarr[j] != arr[j]){
+                count =1 ;
+                break;
+            }   
+        }
+        if (count == 1 ){ System.out.println("The array is not sorted");}
+        else { System.out.println("The array is sorted");}
+    
+        sc.close();
+}
+}
